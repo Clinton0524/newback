9ebabace2d882fdd30fe608a3d2b4b9f4e87c5d0
Clinton0524 <clintonantony111@gmail.com>
2025-03-29 00:28:59 +0530

    first commits

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error('MongoDB Connection Error:', err));
+
 
 // Routes
 app.get('/', (req, res) => {
