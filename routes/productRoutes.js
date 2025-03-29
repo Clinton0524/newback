0e6abe73e8f910bd56783ac550205b593f778a2a
Clinton0524 <clintonantony111@gmail.com>
2025-03-29 13:34:20 +0530

    Your commit messagesssc

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
-const mongoose = require('mongoose'); // ✅ Import mongoose
-
 
 // Get all products
 router.get('/', async (req, res) => {
   try {
-    const products = await Product.find().populate('category').exec();
+    const products = await Product.find().populate('category');
     res.json(products);
   } catch (err) {
     res.status(500).json({ error: err.message });
