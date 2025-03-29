58d39e2ce4334e10b4c341dc817beacb0aab4beb
Clinton0524 <clintonantony111@gmail.com>
2025-03-29 12:30:43 +0530

    Your commit messagess

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -13,14 +13,18 @@ router.get('/', async (req, res) => {
 });
 
 // Create a new product
-router.post('/', async (req, res) => {
-  try {
-    const newProduct = new Product(req.body);
-    await newProduct.save();
-    res.status(201).json(newProduct);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
+router.post("/", async (req, res) => {
+    try {
+      // Convert category to ObjectId
+      req.body.category = new mongoose.Types.ObjectId(req.body.category);
+  
+      const newProduct = new Product(req.body);
+      await newProduct.save();
+  
+      res.status(201).json(newProduct);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
 
 module.exports = router;
