adb81962cc7351a07be675808700122e2df56e5b
Clinton0524 <clintonantony111@gmail.com>
2025-04-01 20:07:15 +0530

    Fixed start script for Render deploymenttssdcfdfd

--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  oldPrice:{type:Number},
+  oldprice:{type:Number},
   category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true }, // Link to Category
   description: { type: String },
   stock: { type: Number, default: 0 },
