93054d932d77d8cec7a9e0877a8196c964eac6c8
Clinton0524 <clintonantony111@gmail.com>
2025-04-02 13:02:13 +0530

    Fixed start script for Render deploymenttssdcfdfd

--- models/Category.js
+++ models/Category.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
   name: { type: String, required: true },
-  isExclusive: { type: Boolean, default: false } // Add this field
+ 
 });
 
 module.exports = mongoose.model('Category', categorySchema);
